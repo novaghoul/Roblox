0102e0b013923c3ddbec03bb55433fbd8532fea1
novaghoul <novaghoul@gmail.com>
2021-12-02 18:17:05 +0700

    new

--- Script/WS Other.lua
+++ Script/WS Other.lua
@@ -1,7 +1,5 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/novaghoul/Roblox/main/Script/1st.lua"), true))() -- 1st Lua
 
-_G.ws_g = 20
-
 _G.down = false
 _G.velocity = Instance.new("BodyVelocity")
 _G.velocity.maxForce = Vector3.new(100000, 0, 100000)
@@ -21,14 +19,14 @@ function onButton1Down(v, g, d)
             v.Parent = lplr.Character.UpperTorso
             g.Parent = lplr.Character.UpperTorso
         end
-        v.velocity = (lplr.Character.Humanoid.MoveDirection) * _G.ws_g
+        v.velocity = (lplr.Character.Humanoid.MoveDirection) * ws_g
         while d do
             if not d then
                 break
             end
             if lplr.Character then
                 if lplr.Character:FindFirstChild("UpperTorso") then
-                    v.velocity = (lplr.Character.Humanoid.MoveDirection) * _G.ws_g
+                    v.velocity = (lplr.Character.Humanoid.MoveDirection) * ws_g
                     local refpos = g.Parent.Position + (g.Parent.Position - wp.CurrentCamera.CoordinateFrame.p).unit * 5
                     g.cframe = CFrame.new(g.Parent.Position, Vector3.new(refpos.x, g.Parent.Position.y, refpos.z))
                 end
@@ -60,13 +58,13 @@ end)
 
 function changeDA(typeDA)
     if typeDA == 0 then
-        _G.ws_g = _G.ws_g + 5
+        ws_g = ws_g + 5
     elseif typeDA == 1 then
-        if _G.ws_g >= 0 then
-            _G.ws_g = _G.ws_g - 5
+        if ws_g >= 0 then
+            ws_g = ws_g - 5
         end
-        if _G.ws_g < 0 then
-            _G.ws_g = 0
+        if ws_g < 0 then
+            ws_g = 0
         end
     end
 end
@@ -74,24 +72,25 @@ end
 mouse.KeyDown:connect(function(keyDown)
     if keyDown == "c" then
         changeDA(0)
-        notify("Dash :" .. _G.ws_g)
+        notify("Dash", ws_g)
     end
     if keyDown == "v" then
         changeDA(1)
-        notify("Dash :" .. _G.ws_g)
+        notify("Dash", ws_g)
     end
     if keyDown == "x" then
-        if _G.ws_g == 20 then
-            _G.ws_g = 17
-            SendChat("Dash :" .. _G.ws_g)
+        if ws_g == 20 then
+            ws_g = 17
+            SendChat("Dash", ws_g)
         else
-            _G.ws_g = 20
-            SendChat("Dash :" .. _G.ws_g)
+            ws_g = 20
+            SendChat("Dash", ws_g)
         end
     end
     if keyDown == "q" then
     end
 end)
+
 spawn(
 	function()
         onButton1Down(_G.velocity, _G.gyro, _G.down)
