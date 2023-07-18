34d688de77e57e24ec8503aff789ea613e070332
novaghoul <novaghoul@gmail.com>
2023-07-18 14:44:32 +0700

    news

--- Script/AimbotGames.lua
+++ Script/AimbotGames.lua
@@ -1,6 +1,6 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/novaghoul/Roblox/main/Script/1st.lua"), true))() -- 1st Lua
 
-function nameGame(name, char)
+getgenv().nameGame = function(name, char)
     if tostring(name) == "Prison Life v2" then
         if (((tostring(char.Team)  == "Criminals" or tostring(char.Team)  == "Inmates") and (tostring(lplr.Team)  == "Criminals" or tostring(lplr.Team)  == "Inmates")) or (tostring(char.Team)  == "Guards" and tostring(lplr.Team)  == "Guards")) then
             return true
