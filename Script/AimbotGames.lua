d90789cfe35310d4b50617a11f1bcc1245f88c6f
novaghoul <novaghoul@gmail.com>
2023-07-18 22:50:13 +0700

    newsas

--- Script/AimbotGames.lua
+++ Script/AimbotGames.lua
@@ -5,7 +5,6 @@ getgenv().nameGame = function(name, char)
         print("-----------------------------------")
         print("-----------------------------------")
         print("-----------------------------------")
-        print("-----------------------------------")
         if tostring(char.Team) == tostring(lplr.Team) then
             return true
         else
