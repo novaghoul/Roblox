f6aa7ab9430891117ac4aba26359c697621b7445
novaghoul <novaghoul@gmail.com>
2023-07-18 22:57:30 +0700

    news

--- Script/AimbotGames.lua
+++ Script/AimbotGames.lua
@@ -4,14 +4,10 @@ getgenv().nameGame = function(name, char)
     if name == "Prison Life v2" then
         if tostring(char.Team) == tostring(lplr.Team) then
             return true
-        else
-            return false
         end
     elseif tostring(name) == "Prison Life v3" then
         if (((tostring(char.Team)  == "Criminals" or tostring(char.Team)  == "Inmates") and (tostring(lplr.Team)  == "Criminals" or tostring(lplr.Team)  == "Inmates")) or (tostring(char.Team)  == "Guards" and tostring(lplr.Team)  == "Guards")) then
             return true
-        else
-            return false
         end
     end
 end
