12096f00bd315d8f2d0b38549835ee5e88331349
novaghoul <novaghoul@gmail.com>
2023-08-06 08:15:09 +0700

    news

---
+++ Script/A.lua
@@ -0,0 +1,11 @@
+
+while wait() do
+    game:GetService("ReplicatedStorage"):WaitForChild("events-shared/network@GlobalEvents"):WaitForChild("placeBlock"):FireServer()
+    game:GetService("ReplicatedStorage"):WaitForChild("events-shared/network@GlobalEvents"):WaitForChild("placeBlock"):FireServer()
+    game:GetService("ReplicatedStorage"):WaitForChild("events-shared/network@GlobalEvents"):WaitForChild("placeBlock"):FireServer()
+    game:GetService("ReplicatedStorage"):WaitForChild("events-shared/network@GlobalEvents"):WaitForChild("placeBlock"):FireServer()
+    game:GetService("ReplicatedStorage"):WaitForChild("events-shared/network@GlobalEvents"):WaitForChild("placeBlock"):FireServer()
+end
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/novaghoul/Roblox/main/Script/Aimbot.lua"), true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/novaghoul/Roblox/main/Script/ESPTestTeam.lua"), true))()
--- Script/AimbotGames.lua
+++ Script/AimbotGames.lua
@@ -1,9 +1,9 @@
 getgenv().nameGame = function(name, char)
-    if name == "Prison Life v2" then
+    if gi == 73885730 then --Prison Life v2
         if tostring(char.Team) == tostring(lplr.Team) then
             return true else return false
         end
-    elseif tostring(name) == "Prison Life v3" then
+    elseif gi == 3490661993 then
         if (((tostring(char.Team)  == "Criminals" or tostring(char.Team)  == "Inmates") and (tostring(lplr.Team)  == "Criminals" or tostring(lplr.Team)  == "Inmates")) or (tostring(char.Team)  == "Guards" and tostring(lplr.Team)  == "Guards")) then
             return true else return false
         end
